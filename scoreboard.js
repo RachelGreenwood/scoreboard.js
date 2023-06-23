444bf1bb3cdde948b316e12a09a66bc315c5b01a
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-06-23 13:52:53 -0400

    Added a way to add players to the scoreboard

--- scoreboard.js
+++ scoreboard.js
@@ -1,5 +1,16 @@
-function createScoreBoard() {
+function createScoreBoard({player, score}) {
     let obj = {
         "The Best Ever": "1000000"
     }
+  obj.name = player;
+  obj.score = score;
+  return obj;
 }
+
+function addPlayer(scoreboard, player, score) {
+  scoreboard[player] = score;
+  return scoreboard;
+  //return createScoreBoard({player, score})
+}
+
+console.log(addPlayer({ 'Dave Thomas': 0 }, 'José Valim', 486373))
